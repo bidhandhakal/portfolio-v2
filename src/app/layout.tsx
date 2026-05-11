102c595f56176eb015e1134a6117d7a4141148c2
Bidhaan Dhakal <bidhandhakal.web@gmail.com>
2026-05-11 12:47:58 +0545

    Add Open Graph and Twitter metadata to portfolio
    - Add og:title, og:description, og:url, og:type, og:image meta tags
    - Add twitter:card, twitter:title, twitter:description, twitter:image meta tags
    - Use absolute URLs with bidhandhakal.com.np domain
    - Configure og-image.png as social sharing image (1200x630)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,27 @@ const hankenGrotesk = localFont({
 
 export const metadata: Metadata = {
   title: "Bidhaan",
-  description: "idk",
+  description: "Building simple software, usually.",
+  openGraph: {
+    title: "Bidhaan",
+    description: "Building simple software, usually.",
+    url: "https://bidhandhakal.com.np",
+    type: "website",
+    images: [
+      {
+        url: "https://bidhandhakal.com.np/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Bidhaan",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Bidhaan",
+    description: "Building simple software, usually.",
+    images: ["https://bidhandhakal.com.np/og-image.png"],
+  },
 };
 
 export default function RootLayout({
